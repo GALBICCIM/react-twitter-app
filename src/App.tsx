4dc72d27de5d5875d4584a1922654aa0a6475b7c
GALBICCIM <godvictory0909@gmail.com>
2024-03-09 22:31:53 +0900

    Add foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,57 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import CreateAccount from "./routes/Create-account";
+import { Login } from "./routes/Login";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <Layout />,
+      children: [
+         {
+            path: "",
+            element: <Home />,
+         },
+         {
+            path: "profile",
+            element: <Profile />,
+         },
+      ],
+   },
+   {
+      path: "/login",
+      element: <Login />,
+   },
+   {
+      path: "/create-account",
+      element: <CreateAccount />,
+   },
+]);
+
+const GlobalStyle = createGlobalStyle`
+   ${reset};
+   * {
+
+   }
+
+   body {
+      background-color: black;
+      color: white;
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+   }
+`;
+
 const App = () => {
-   return <></>;
+   return (
+      <>
+         <GlobalStyle />
+         <RouterProvider router={router} />
+      </>
+   );
 };
 
 export default App;
---
+++ src/routes/Create-account.tsx
@@ -0,0 +1,5 @@
+const CreateAccount = () => {
+   return <h1>Create your account</h1>;
+};
+
+export default CreateAccount;
