f8853116d3dd043a51f4a35185b9564ea389253f
GALBICCIM <godvictory0909@gmail.com>
2024-03-19 18:24:14 +0900

    recommit before

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,75 +1,75 @@
 import { addDoc, collection, updateDoc } from "firebase/firestore";
-import * as Style from "../styles/TweetStyle";
+import * as Style from "../styles/WriteTweetStyle";
 import { useState } from "react";
 import { auth, db, storage } from "../firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export const PostTweetForm = () => {
-	const [isLoading, setLoading] = useState(false);
-	const [tweet, setTweet] = useState("");
-	const [file, setFile] = useState<File | null>(null);
-	const [error, setError] = useState("");
+   const [isLoading, setLoading] = useState(false);
+   const [tweet, setTweet] = useState("");
+   const [file, setFile] = useState<File | null>(null);
+   const [error, setError] = useState("");
 
-	const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-		setTweet(event.target.value);
-	};
+   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setTweet(event.target.value);
+   };
 
-	const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const { files } = event.target;
-		if (files && files.length === 1) {
-			setFile(files[0]);
-		}
-	};
+   const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { files } = event.target;
+      if (files && files.length === 1) {
+         setFile(files[0]);
+      }
+   };
 
-	const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		const user = auth.currentUser; // 현재 로그인되고 있는 유저
+   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const user = auth.currentUser; // 현재 로그인되고 있는 유저
 
-		if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+      if (!user || isLoading || tweet === "" || tweet.length > 180) return;
 
-		try {
-			setLoading(true);
+      try {
+         setLoading(true);
 
-			// 어떤 db에 저장할지, 그 안에 무슨 인스턴스를 저장할지
-			const doc = await addDoc(collection(db, "tweets"), {
-				tweet,
-				createdAt: Date.now(),
-				username: user.displayName || "Anonymous",
-				userId: user.uid
-			});
+         // 어떤 db에 저장할지, 그 안에 무슨 인스턴스를 저장할지
+         const doc = await addDoc(collection(db, "tweets"), {
+            tweet,
+            createdAt: Date.now(),
+            username: user.displayName || "Anonymous",
+            userId: user.uid,
+         });
 
-			if (file) {
-				if (file.size > 1000000) {
-					setError("The file is to large. (Less than 1MB");
-					return;
-				} else {
-					const locationRef = ref(storage, `tweets/${user.uid}-${user.displayName}/${doc}`);
-					const result = await uploadBytes(locationRef, file);
-					const url = await getDownloadURL(result.ref);
+         if (file) {
+            if (file.size > 1000000) {
+               setError("The file is to large. (Less than 1MB");
+               return;
+            } else {
+               const locationRef = ref(storage, `tweets/${user.uid}-${user.displayName}/${doc}`);
+               const result = await uploadBytes(locationRef, file);
+               const url = await getDownloadURL(result.ref);
 
-					await updateDoc(doc, {
-						photo: url
-					});
-				}
-			}
-		} catch (e) {
-			setTweet("");
-			setError("");
-			setFile(null);
+               await updateDoc(doc, {
+                  photo: url,
+               });
+            }
+         }
+      } catch (e) {
+         setTweet("");
+         setError("");
+         setFile(null);
 
-			console.log(e);
-		} finally {
-			setLoading(false);
-		}
-	};
+         console.log(e);
+      } finally {
+         setLoading(false);
+      }
+   };
 
-	return (
-		<Style.Form onSubmit={onSubmit}>
-			<Style.TextArea onChange={onChange} rows={5} maxLength={180} placeholder="What is happening?" required />
-			<Style.AttachFileButton htmlFor="file">{file ? "Photo Added" : "Add photo"}</Style.AttachFileButton>
-			<Style.AttachFileInput onChange={onFileChange} id="file" type="file" accept="image/*" />
-			<Style.SubmitButton type="submit" value={isLoading ? "Posting..." : "Post Tweet"} />
-			{error !== "" ? <Style.Error>{error}</Style.Error> : null}
-		</Style.Form>
-	);
+   return (
+      <Style.Form onSubmit={onSubmit}>
+         <Style.TextArea onChange={onChange} rows={5} maxLength={180} placeholder="What is happening?" required />
+         <Style.AttachFileButton htmlFor="file">{file ? "Photo Added" : "Add photo"}</Style.AttachFileButton>
+         <Style.AttachFileInput onChange={onFileChange} id="file" type="file" accept="image/*" />
+         <Style.SubmitButton type="submit" value={isLoading ? "Posting..." : "Post Tweet"} />
+         {error !== "" ? <Style.Error>{error}</Style.Error> : null}
+      </Style.Form>
+   );
 };
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { Wrapper } from "../styles/TimelineStyle";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, limit, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../firebase";
+import { Tweets } from "./tweets";
+import { Unsubscribe } from "firebase/auth";
 
 export interface ITweet {
-   photo: string;
+   id: string;
+   photo?: string;
    tweet: string;
    userId: string;
    username: string;
@@ -14,15 +17,39 @@ export interface ITweet {
 export const Timeline = () => {
    const [tweets, setTweets] = useState<ITweet[]>([]);
 
-   // 트윗 불러오기
-   const fetchTweets = async () => {
-      const tweetsQuery = query(collection(db, "tweets"), orderBy("createdAt", "desc"));
-      const snapshot = await getDocs(tweetsQuery);
-      snapshot.docs.forEach((doc) => console.log(doc.data()));
-   };
-
    useEffect(() => {
+      let unsubscribe: Unsubscribe | null = null;
+      // 트윗 불러오기
+      const fetchTweets = async () => {
+         const tweetsQuery = query(collection(db, "tweets"), orderBy("createdAt", "desc"), limit(15)); // 쿼리 생성
+
+         // 이벤트 리스너 삽입
+         unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+            const tweets = snapshot.docs.map((doc) => {
+               const { photo, tweet, userId, username, createAt } = doc.data();
+               return {
+                  id: doc.id,
+                  photo,
+                  tweet,
+                  userId,
+                  username,
+                  createAt,
+               };
+            });
+            setTweets(tweets);
+         });
+      };
       fetchTweets();
+
+      return () => {
+         unsubscribe && unsubscribe();
+      };
    }, []);
-   return <Wrapper>{JSON.stringify(tweets)}</Wrapper>;
+   return (
+      <Wrapper>
+         {tweets.map((tweet) => (
+            <Tweets key={tweet.id} {...tweet} />
+         ))}
+      </Wrapper>
+   );
 };
