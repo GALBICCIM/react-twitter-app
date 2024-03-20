e4c45119703a45e00f5237d6080e152380c8cbe4
GALBICCIM <godvictory0909@gmail.com>
2024-03-20 17:22:14 +0900

    Done for edit tweet.

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,58 +1,82 @@
 import { ITweet } from "./timeline";
 import * as Style from "../styles/TweetFieldStyle";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 import { useState } from "react";
 
 export const Tweet = ({ username, photo, tweet, userId, id }: ITweet) => {
-	const [isEditing, setEditing] = useState(false);
-	const [editTweet, setEditTweet] = useState("");
-	const user = auth.currentUser;
+   const [isEditing, setEditing] = useState(false);
+   const [editTweet, setEditTweet] = useState(tweet);
+   const user = auth.currentUser;
 
-	const onEdit = async () => {
-		if (user?.uid !== userId) return;
+   const handleEditing = async () => {
+      if (user?.uid !== userId) return;
 
-		setEditing((prev) => !prev);
-	};
+      setEditing((prev) => !prev);
+   };
 
-	const onDelete = async () => {
-		const ok = confirm("Are you sure you want to delete this tweet?");
+   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setEditTweet(event.target.value);
+   };
 
-		// 지우는 것에 동의하지 않으면, 로그인 된 유저와 트윗 작성자의 아이디가 다를 때 함수 종료
-		if (!ok || user?.uid !== userId) return;
+   const onEditDone = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
 
-		try {
-			await deleteDoc(doc(db, "tweets", id)); // 트윗 삭제
+      try {
+         await updateDoc(doc(db, "tweets", id), {
+            tweet: editTweet,
+         });
+      } catch (e) {
+         console.log(e);
+      } finally {
+         setEditing((prev) => !prev);
+      }
+   };
 
-			// 만약 사진이 존재하면 사진도 같이 삭제
-			if (photo) {
-				const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
-				await deleteObject(photoRef);
-			}
-		} catch (e) {
-			console.log(e);
-		} finally {
-		}
-	};
+   const onDelete = async () => {
+      const ok = confirm("Are you sure you want to delete this tweet?");
 
-	return (
-		<Style.Wrapper>
-			<Style.Columns>
-				<Style.Username>{username}</Style.Username>
-				{isEditing ? <Style.EditInput type="text" value={editTweet}></Style.EditInput> : <Style.Payload>{tweet}</Style.Payload>}
-				{user?.uid === userId ? (
-					<Style.Columns>
-						<Style.DeleteButton onClick={onDelete}>Delete</Style.DeleteButton>
-						<Style.EditButton onClick={onEdit}>{isEditing ? "Confirm" : "Edit"}</Style.EditButton>
-					</Style.Columns>
-				) : null}
-			</Style.Columns>
-			{photo ? (
-				<Style.Columns>
-					<Style.Photo src={photo} />
-				</Style.Columns>
-			) : null}
-		</Style.Wrapper>
-	);
+      // 지우는 것에 동의하지 않으면, 로그인 된 유저와 트윗 작성자의 아이디가 다를 때 함수 종료
+      if (!ok || user?.uid !== userId) return;
+
+      try {
+         await deleteDoc(doc(db, "tweets", id)); // 트윗 삭제
+
+         // 만약 사진이 존재하면 사진도 같이 삭제
+         if (photo) {
+            const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+            await deleteObject(photoRef);
+         }
+      } catch (e) {
+         console.log(e);
+      }
+   };
+
+   return (
+      <Style.Wrapper>
+         <Style.Columns>
+            <Style.Username>{username}</Style.Username>
+            {isEditing ? (
+               <Style.Form onSubmit={onEditDone}>
+                  <Style.EditTextArea onChange={onChange} value={editTweet} placeholder="Correct your writing here."></Style.EditTextArea>
+                  <Style.EditSubmit type="submit" value="Done"></Style.EditSubmit>
+               </Style.Form>
+            ) : (
+               <Style.Payload>{tweet}</Style.Payload>
+            )}
+            {user?.uid === userId ? (
+               <Style.Columns>
+                  <Style.DeleteButton onClick={onDelete}>Delete</Style.DeleteButton>
+                  {isEditing ? null : <Style.EditButton onClick={handleEditing}>Edit</Style.EditButton>}
+               </Style.Columns>
+            ) : null}
+         </Style.Columns>
+         {photo ? (
+            <Style.Columns>
+               <Style.Photo src={photo} />
+            </Style.Columns>
+         ) : null}
+      </Style.Wrapper>
+   );
 };
